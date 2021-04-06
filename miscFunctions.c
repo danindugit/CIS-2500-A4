a057731638baba30b9c5a7d9ce732d4606241ef0
Danindu Marasinghe <dmarasin@uoguelph.ca>
2021-04-06 17:20:06 -0400

    Setup functions

--- miscFunctions.c
+++ miscFunctions.c
@@ -1,1 +1,28 @@
 #include "headerA4.h"
+
+/******
+ sortID:  sorts tweetlist by ID
+ In: the head and tail nodes
+ Out: none
+*******/
+void sortID (tweet ** head, tweet ** tail){
+
+}
+
+/******
+ reverse:  reverses tweetlist
+ In: the head and tail nodes
+ Out: none
+*******/
+void reverse (tweet ** head, tweet ** tail){
+
+}
+
+/******
+ sortUsername:  sorts tweetlist by ID
+ In: the head and tail nodes
+ Out: none
+*******/
+void sortUsername (tweet ** head, tweet ** tail){
+
+}

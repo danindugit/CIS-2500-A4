224ad27a47347014492d5f327218ad20ec8a5c1e
Danindu Marasinghe <dmarasin@uoguelph.ca>
2021-04-06 00:40:09 -0400

    structure + includes

---
+++ headerA4.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//structure definition
+typedef struct microtweet {
+    int id; //non-unique integer value
+    char user [51]; // the username / userid of the person who wrote the tweet
+    char text [141]; // the text of the tweet
+    struct microtweet *next; //dynamic connection to the next tweet
+} tweet;
+
+//function prototypes
+void enqueue (tweet ** head, tweet ** tail, tweet * node);
+void dequeue (tweet ** head, tweet ** tail);
+int isEmpty (tweet * head);
+void printQueue (tweet * head);
+void sortID (tweet ** head, tweet ** tail);
+void reverse (tweet ** head, tweet ** tail);
+void sortUsername (tweet ** head, tweet ** tail);

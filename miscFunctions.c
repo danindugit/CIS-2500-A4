d1dea36f42a8122fa890756d3980f90efe582422
Danindu Marasinghe <dmarasin@uoguelph.ca>
2021-04-06 21:31:54 -0400

    Done sortID

--- miscFunctions.c
+++ miscFunctions.c
@@ -8,16 +8,30 @@
 void sortID (tweet ** head, tweet ** tail){
     tweet* ptr1;
     tweet* ptr2;
-    tweet* temp;
+    tweet* tempTail;
+
+    tempTail = *tail;
+    ptr1 = *head;
+    ptr2 = *head;
 
     if(isEmpty(*head)){
         printf("Cannot sort because this tweet list is empty.\n");
         return;
     }
     //bubble sort
-    while(ptr1 != NULL){
-        
+    while(ptr1->next != NULL){
+        ptr2 = *head;
+        while(ptr2 != tempTail){
+            if(ptr2->id > ptr2->next->id){
+                swap(ptr2, ptr2->next);
+            }
+            ptr2 = ptr2->next;
+        }
+        tempTail = ptr2;
+        ptr1 = ptr1->next;
     }
+
+    printf("Successfully Sorted by ID.\n");
 }
 
 /******
